09eff440c1ed23da1cd33e006b6e5cb0c41c5bf1
yasheks <143996832+yasheks@users.noreply.github.com>
2023-10-19 23:17:38 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -15,4 +15,5 @@ bool itc_equal_reverse(string str);
 string itc_cmp_str(string str1, string str2, int num);
 int itc_find_str(string str1, string str2);
 string itc_three_str(string str1, string str2, string str3);
+int itc_max_char_on_end(string str);
 #endif // STR_EASY_H_INCLUDED
